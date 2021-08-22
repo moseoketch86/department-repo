5dbcdb8cba651eef8f7eb0d1360f63d2ab79ef02
Your Name <moseoduor86@gmail.com>
2021-08-22 13:11:11 +0300

    added endpoints for get and get by id

--- src/main/java/com/demo/department/controller/DepartmentController.java
+++ src/main/java/com/demo/department/controller/DepartmentController.java
@@ -3,9 +3,9 @@ package com.demo.department.controller;
 import com.demo.department.entity.Department;
 import com.demo.department.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -16,5 +16,13 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
        return departmentService.saveDepartment(department);
+    }
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+    @GetMapping("departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id")Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
     }
 }
--- src/main/java/com/demo/department/service/DepartmentService.java
+++ src/main/java/com/demo/department/service/DepartmentService.java
@@ -2,6 +2,12 @@ package com.demo.department.service;
 
 import com.demo.department.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
    public Department saveDepartment(Department department);
+
+  public List<Department> fetchDepartmentList();
+
+   public Department fetchDepartmentById(Long departmentId);
 }
